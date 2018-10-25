35c938995a6dd876de62bfa2f947fb4df5f58c93
bdarbog <bdarbog@bdarbog.it.pointpark.edu>
2018-10-25 19:03:31 -0400

    updated dnd.js file

--- dnd.js
+++ dnd.js
@@ -1,7 +1,10 @@
 var express = require('express');
 
 var app = express();
-
+var handlebars = require('express-handlebars')                                 
+        .create({ defaultLayout:'main' });                                     
+app.engine('handlebars', handlebars.engine);                                  
+app.set('view engine', 'handlebars'); 
 app.set('port', process.env.PORT || 3000);
 
 app.use(function(req, res, next){
@@ -33,37 +36,6 @@ app.use(function(err, req, res, next){
         console.error(err.stack);
         res.status(500);
         res.render('500');
-});
-
-//set up handlebars view engine
-var handlebars = require('express-handlebars')
-        .create({ defaultLayout:'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-
-app.get('/', function(req, res){
-        res.type('text/plain');
-        res.send('Meadowlark Travel');
-});
-app.get('/about', function(req, res){
-        res.type('text/plain');
-        res.send('About Meadowlark Travel');
-});
-
-
-// custom 404 page
-app.use(function(req, res, next){
-        res.type('text/plain');
-        res.status(404);
-        res.send('404 - Not Found');
-});
-
-// custom 500 page
-app.use(function(err, req, res, next){
-        console.error(err.stack);
-        res.type('text/plain');
-        res.status(500);
-        res.send('500 - Server Error');
 });
 
 app.listen(app.get('port'), function(){
