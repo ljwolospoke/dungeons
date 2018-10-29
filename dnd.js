511911e3baf5e2554a1e16050e6abc1d20ff9bf4
bdarbog <bdarbog@bdarbog.it.pointpark.edu>
2018-10-29 17:48:07 -0400

    brandon two

--- dnd.js
+++ dnd.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var credentials = require('./credentials.js');
 
 var app = express();
 var handlebars = require('express-handlebars')                                 
@@ -12,12 +13,60 @@ app.use(function(req, res, next){
         next();
 });
 //routes go here
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session') ({
+  resave: false,
+  saveUninitialized: false,
+  secret: credentials.cookieSecret,
+}));
+
+app.get('/headers', function(req, res){
+res.set('Content-Type', 'text/plain');
+var s = '';
+for(var name in req.headers) s += name + ': ' + req.headers[name] + '/n';
+res.send(s);
+});
+
 
 app.get('/', function(req, res) {
-        res.render('home');
+  req.session.userName = 'Brandon';
+  console.log(req.cookies.website);
+  res.cookie('website', 'alert');
+  res.render('home');
 });
+
+app.get('/sign', function(req, res){
+res.render('sign', { csrf: 'CSRF token goes here' });
+});
+
+app.get('/sign-ajax', function(req, res){
+res.render('sign-ajax', { csrf: 'CSRF token goes here' });
+});
+
+
 app.get('/about', function(req, res){
         res.render('about');
+});
+
+app.post('/process', function(req,res){
+  if(req.xhr || req.accepts('json,html')==='json'){
+  // if there were an error, we would send {error: 'error description' }
+  console.log(JSON.stringify(req.body));
+  res.send({
+  success: true,
+  message: "The Submission Was Successful!"
+ });
+} else {
+  // if there were an error, we would redirect to an error page
+  res.redirect(303, '/');
+  }
+});
+
+app.use(function(req, res, next){
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
 });
 
 
